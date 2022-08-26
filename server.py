fd9749bd94a574057d6987e4b4112c6e0c6f3b24
wplfe <abiodunyoung@yahoo.com>
2022-08-26 09:45:20 +0100

    try 4

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
